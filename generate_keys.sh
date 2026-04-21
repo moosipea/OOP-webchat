3aa5a7c0c0cba5967a306ed8b9cbd4235524958d
oskarasd123 <71971772+oskarasd123@users.noreply.github.com>
2026-04-21 13:59:41 +0300

    change key to ecc

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,3 +1,10 @@
-keytool -genkeypair -alias my-server-key -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore keystore.p12 -validity 365
+keytool -genkeypair \
+  -alias my-server-key \
+  -keyalg EC \
+  -groupname secp256r1 \
+  -sigalg SHA256withECDSA \
+  -storetype PKCS12 \
+  -keystore keystore.p12 \
+  -validity 365
 keytool -exportcert -alias my-server-key -keystore keystore.p12 -file server-public-cert.cer
 keytool -importcert -alias my-server-key -file server-public-cert.cer -keystore client-truststore.p12
